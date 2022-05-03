dd813cc1f2ce7407b1b421bbbee19e382efaa61e
Soren Saket <sutensaket@gmail.com>
2022-05-03 14:46:56 +0200

    Game Master

---
+++ GameMaster.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Core;
+using Microsoft.Xna.Framework.Content;
+
+namespace FactoryDefense
+{
+	public class GameMaster : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+		private SceneManager sceneManager;
+
+		
+		/// <summary> The viewport bounds in pixels. </summary>
+		public Vector2 ViewportSize => new Vector2(_graphics.GraphicsDevice.Viewport.Width, _graphics.GraphicsDevice.Viewport.Height);
+		
+
+		public GameMaster()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			sceneManager = new SceneManager(this);
+
+			sceneManager.LoadScene(new GameScene());
+
+			// TODO: use this.Content to load your game content here
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			sceneManager.Update();
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			sceneManager.Draw(_spriteBatch);
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
