c1efcd02317ab321d78fb9c5bc719fda37c39b19
Soren Saket <sutensaket@gmail.com>
2022-05-05 13:45:58 +0200

    Change namespace

--- GameMaster.cs
+++ GameMaster.cs
@@ -3,8 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Core;
 using Microsoft.Xna.Framework.Content;
+using System;
 
-namespace FactoryDefense
+namespace Core
 {
 	public class GameMaster : Game
 	{
@@ -20,9 +21,16 @@ namespace FactoryDefense
 		public GameMaster()
 		{
 			_graphics = new GraphicsDeviceManager(this);
+			_graphics.PreferredBackBufferWidth = 1280;
+			_graphics.PreferredBackBufferHeight = 720;
+
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
+		
+
 			Window.AllowUserResizing = true;
+			Window.ClientSizeChanged += new EventHandler<EventArgs>(Window_ClientSizeChanged);
+
 		}
 
 		protected override void Initialize()
@@ -61,6 +69,39 @@ namespace FactoryDefense
 			// TODO: Add your drawing code here
 
 			base.Draw(gameTime);
+		}
+
+		// Contant window aspect ratio
+		float AspectRatio = 16f / 9f;
+		Point OldWindowSize;
+
+		void Window_ClientSizeChanged(object sender, EventArgs e)
+		{
+			// https://stackoverflow.com/questions/8396677/uniformly-resizing-a-window-in-xna
+
+			// Remove this event handler, so we don't call it when we change the window size in here
+			Window.ClientSizeChanged -= new EventHandler<EventArgs>(Window_ClientSizeChanged);
+
+			if (Window.ClientBounds.Width != OldWindowSize.X)
+			{ // We're changing the width
+			  // Set the new backbuffer size
+				_graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
+				_graphics.PreferredBackBufferHeight = (int)(Window.ClientBounds.Width / AspectRatio);
+			}
+			else if (Window.ClientBounds.Height != OldWindowSize.Y)
+			{ // we're changing the height
+			  // Set the new backbuffer size
+				_graphics.PreferredBackBufferWidth = (int)(Window.ClientBounds.Height * AspectRatio);
+				_graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
+			}
+
+			_graphics.ApplyChanges();
+
+			// Update the old window size with what it is currently
+			OldWindowSize = new Point(Window.ClientBounds.Width, Window.ClientBounds.Height);
+
+			// add this event handler back
+			Window.ClientSizeChanged += new EventHandler<EventArgs>(Window_ClientSizeChanged);
 		}
 	}
 }
