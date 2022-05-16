b0277a574408d4ca907af7da64e90492cecea5d9
Soren Saket <sutensaket@gmail.com>
2022-05-16 15:26:40 +0200

    Update SpriteSheet.cs

--- Other/SpriteSheet.cs
+++ Other/SpriteSheet.cs
@@ -22,25 +22,35 @@ namespace Core
 		/// <summary> The Height of a frame </summary>
 		public int Height { get; private set; }
 
-
 		private readonly Texture2D texture;
 		private readonly Rectangle[] frames;
 		private readonly Vector2 origin;
 
-		public SpriteSheet(Texture2D texture, int columns = 1, int rows = 1, int startElement = 0, int elements = -1)
+		public SpriteSheet(Texture2D texture, int columns = 1, int rows = 1, int startElement = 0, int elements = -1, int elementWidth = 0, int elementHeight = 0)
 		{
 			this.texture = texture;
-		
+
+			// If no element width is set calculate from texture width
+			if (elementWidth <= 0)
+				Width = texture.Width / columns;
+			else
+				Width = elementWidth;
+
+			if (elementHeight <= 0)
+				Height = texture.Height / rows;
+			else
+				Height = elementHeight;
+
+			// stride is always constant based on texture size and columns/rows
+			int strideWidth = texture.Width / columns;
+			int strideHeight = texture.Height / rows;
+
 			// If no element count is already specified uses the whole texture
 			if (elements <= 0)
 				elements = columns * rows;
 
 			// The number of frames to select
 			int frameCount = elements - startElement;
-
-			// The size of a single frame
-			Width = texture.Width / columns;
-			Height = texture.Height / rows;
 
 			// initialize the frames with the computed framecount
 			this.frames = new Rectangle[frameCount]; 
@@ -50,9 +60,8 @@ namespace Core
             {
 				int x = ((startElement + i) % columns);
 				int y = (((startElement + i) / columns));
-				frames[i] = new Rectangle( x * Width, y * Height, Width, Height);
+				frames[i] = new Rectangle( x * strideWidth, y * strideHeight, Width, Height);
 			}
-				
 
 			// Compute origin as center
 			this.origin = new Vector2(Width, Height) / 2f;
